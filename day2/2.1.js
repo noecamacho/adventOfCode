0ecead83a477d76002db93f61c4dae914df0e99b
Bob Laskowski <blaskowski94@gmail.com>
2018-11-10 19:14:05 -0600

    day 2

--- day2/2.1.js
+++ day2/2.1.js
@@ -1,2 +1,23 @@
 const _ = require('lodash')
+const readline = require('readline')
 const fs = require('fs')
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('2.1.input.txt'),
+    crlfDelay: Infinity
+})
+
+let totalPaper = 0
+
+rl.on('line', line => {
+    let dimensions = line.split('x')
+    totalPaper += calculate(parseInt(dimensions[0]), parseInt(dimensions[1]), parseInt(dimensions[2]))
+}).on('close', () => {
+    console.log(totalPaper)
+})
+
+let calculate = (l, w, h) => {
+    let vals = [l, w, h]
+    vals.splice(_.indexOf(vals, _.max(vals)), 1)
+    return (2 * l * w) + (2 * w * h) + (2 * h * l) + (vals[0] * vals[1])
+}
